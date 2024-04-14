cc400e5483213a187f930212d1277329b21c7f16
StanislasMellier <95698594+StanislasMellier@users.noreply.github.com>
2024-04-14 12:52:14 +0200

    Code reorganized and add multiple things
    Add blur around stars
    Add a camera with zoom and can follow the simulation
    Move the GUI in the simulation code
    Add G constant
    Add timeFactor to handle simulation speed

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ class Simulation {
         this.canvas = canvas
         this.ctx = canvas.getContext('2d')
         this.bodies = []
+        this.G = 1
         this.softeningFactor = 20 || softeningFactor
-        this.timeFactor = 1 || timeFactor
+        this.timeFactor = 0.1 || timeFactor
 
+        this.camera = { x: 0, y: 0, zoom: 1 }
+        this.followCenterOfSimulation = false
+
+        this.gui = new dat.GUI()
+        this.guiElement = {}
         window.addEventListener('resize', () => {
             this.canvas.width = window.innerWidth
             this.canvas.height = window.innerHeight
@@ -31,91 +37,135 @@ class Simulation {
         this.init()
     }
     init() {
+        let SimulationFolder = this.gui.addFolder("Simulation");
+        SimulationFolder.open()
 
-        let xCenter = this.ctx.canvas.width * 0.5;
-        let yCenter = this.ctx.canvas.height * 0.5;
+        let cameraZoomController = SimulationFolder.add(this.camera, 'zoom', 0, 1);
+        cameraZoomController.name('Camera zoom');
+        let softeningController = SimulationFolder.add(this, 'softeningFactor', 0, 100);
+        let timeFactorController = SimulationFolder.add(this, 'timeFactor', 0.01, 1.00);
+        let FollowCenterController = SimulationFolder.add(this, 'followCenterOfSimulation');
+        FollowCenterController.name('Camera centered');
 
-        this.addBody(xCenter, yCenter, null, 5, true, "yellow");
-        this.addBody(xCenter, yCenter - yCenter * 0.5, { x: 2, y: 0 }, 1);
-        this.addBody(xCenter, yCenter - yCenter * -0.5, { x: -2, y: 0 }, 1);
-        this.addBody(xCenter, yCenter - yCenter * -0.55, { x: -2.7, y: 0 }, 0.00000001);
+        this.guiElement = { ...this.guiElement, SimulationFolder, cameraZoomController, softeningController, timeFactorController, FollowCenterController }
+
+        this.loadExemple()
+        this.handleMouseAddBody()
 
     }
     addBody(x, y, velocity, mass, forzen, color) {
         this.bodies.push(new Body(x, y, velocity, mass, forzen, color));
     }
+    handleMouseAddBody() {
+        let body = { x: 0, y: 0, velocity: { x: 0, y: 0 }, mass: 1, color: '#ffffff', frozen: false }
+
+        let AddBodyParameterFoler = this.gui.addFolder("Add Body Parameter");
+        AddBodyParameterFoler.open()
+
+        let AddBodyMassController = AddBodyParameterFoler.add(body, 'mass')
+        let AddBodyColorController = AddBodyParameterFoler.addColor(body, 'color')
+        let AddBodyFrozenController = AddBodyParameterFoler.add(body, 'frozen')
+
+        this.guiElement = { ...this.guiElement, AddBodyParameterFoler, AddBodyMassController, AddBodyColorController, AddBodyFrozenController }
+
+        this.canvas.addEventListener('mousedown', (e) => {
+            let clickXFromCanvaCenter = e.offsetX - (this.canvas.width / 2)
+            let clickYFromCanvaCenter = e.offsetY - (this.canvas.height / 2)
+            body.x = this.camera.x + (clickXFromCanvaCenter / this.camera.zoom)
+            body.y = this.camera.y + (clickYFromCanvaCenter / this.camera.zoom)
+        })
+        this.canvas.addEventListener('mouseup', (e) => {
+            if (!body) return
+
+            let clickXFromCanvaCenter = e.offsetX - (this.canvas.width / 2)
+            let clickYFromCanvaCenter = e.offsetY - (this.canvas.height / 2)
+
+            let clickXCoordInSimulation = this.camera.x + (clickXFromCanvaCenter / this.camera.zoom)
+            let clickYCoordInSimulation = this.camera.y + (clickYFromCanvaCenter / this.camera.zoom)
+
+            body.velocity.x = (body.x - clickXCoordInSimulation) * 0.05
+            body.velocity.y = (body.y - clickYCoordInSimulation) * 0.05
+            this.addBody(body.x, body.y, { x: body.velocity.x, y: body.velocity.y }, body.mass, body.frozen, body.color)
+        })
+    }
+
+    loadExemple() {
+
+        this.addBody(0, 0, null, 10, true, "yellow");
+        this.addBody(0, -250, { x: 3.25, y: 0 }, 1);
+        this.addBody(0, 250, { x: -3.25, y: 0 }, 1);
+        this.addBody(0, 260, { x: -4.0, y: 0 }, 0.001);
+
+    }
     update(delta) {
+        this.CenterOfSimulation = { x: 0, y: 0 }
         for (let i = 0; i < this.bodies.length; i++) {
             let body = this.bodies[i]
 
+            let a = { x: 0, y: 0 }
             if (body.frozen === false) {
-                let a = { x: 0, y: 0 }
                 for (let j = 0; j < this.bodies.length; j++) {
                     let otherBody = this.bodies[j]
 
                     if (body !== otherBody) {
                         let diff = { x: otherBody.x - body.x, y: otherBody.y - body.y }
+                        let d2 = Math.sqrt(diff.x ** 2 + diff.y ** 2 + this.softeningFactor) ** 2
 
-                        let d2 = Math.sqrt(diff.x ** 2 + diff.y ** 2) ** 2
-
-                        a.x += diff.x * otherBody.mass / d2
-                        a.y += diff.y * otherBody.mass / d2
+                        a.x += this.G * diff.x * otherBody.mass / d2
+                        a.y += this.G * diff.y * otherBody.mass / d2
                     }
                 }
 
-                body.velocity.x += a.x * delta
-                body.velocity.y += a.y * delta
+                body.velocity.x += a.x * (delta * this.timeFactor)
+                body.velocity.y += a.y * (delta * this.timeFactor)
 
-                body.x += body.velocity.x * delta
-                body.y += body.velocity.y * delta
+                body.x += body.velocity.x * (delta * this.timeFactor)
+                body.y += body.velocity.y * (delta * this.timeFactor)
             }
+            if (this.followCenterOfSimulation === true) {
+                this.CenterOfSimulation.x += body.x
+                this.CenterOfSimulation.y += body.y
+            }
+        }
+
+        if (this.followCenterOfSimulation === true) {
+            this.CenterOfSimulation.x = this.CenterOfSimulation.x / this.bodies.length
+            this.CenterOfSimulation.y = this.CenterOfSimulation.y / this.bodies.length
+
+            this.camera.x = this.CenterOfSimulation.x
+            this.camera.y = this.CenterOfSimulation.y
         }
     }
     draw() {
-        this.ctx.fillStyle = "rgba(0,0,0,1)";
+        this.ctx.fillStyle = "rgba(0,0,0,1.0)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         for (let i = 0; i < this.bodies.length; i++) {
             let body = this.bodies[i];
 
-            let radius = body.mass * 2 + this.canvas.width * 0.001
+            let objectXfromCam = (body.x - this.camera.x) * this.camera.zoom
+            let objectYfromCam = (body.y - this.camera.y) * this.camera.zoom
+
+            let objectXOnCanva = objectXfromCam + this.canvas.width / 2
+            let objectYOnCanva = objectYfromCam + this.canvas.height / 2
+
+            let radius = (body.mass * 2) * this.canvas.width * 0.0005 + this.canvas.width * 0.0005
+
             this.ctx.fillStyle = `${body.color}`;
+
+            this.ctx.filter = `blur(${radius * Math.ceil(this.camera.zoom)}px)`;
             this.ctx.beginPath();
-            this.ctx.arc(body.x, body.y, radius, 0, Math.PI * 2);
+            this.ctx.arc(objectXOnCanva, objectYOnCanva, radius * this.camera.zoom, 0, Math.PI * 2);
+            this.ctx.fill()
+
+            this.ctx.filter = 'none';
+            this.ctx.beginPath();
+            this.ctx.arc(objectXOnCanva, objectYOnCanva, radius * this.camera.zoom, 0, Math.PI * 2);
             this.ctx.fill()
         }
     }
 }
 const simulation = new Simulation(canvas);
-
-let body = { x: 0, y: 0, velocity: { x: 0, y: 0 }, mass: 1, color: '#ffffff', frozen: false }
-
-let gui = new dat.GUI()
-
-let SimulationFolder = gui.addFolder("Simulation");
-SimulationFolder.open()
-let softeningController = SimulationFolder.add(simulation, 'softeningFactor', 0, 100);
-let centerStarController = SimulationFolder.add(simulation.bodies[0], 'mass', 0, 100);
-centerStarController.name("Mass of the  star")
-let AddBodyParameterFoler = gui.addFolder("Add Body Parameter");
-AddBodyParameterFoler.open()
-
-let AddBodyMassController = AddBodyParameterFoler.add(body, 'mass', 0, 100)
-let AddBodyColorController = AddBodyParameterFoler.addColor(body, 'color')
-let AddBodyFrozenController = AddBodyParameterFoler.add(body, 'frozen')
-
-
-canvas.addEventListener('mousedown', (e) => {
-    body.x = e.offsetX
-    body.y = e.offsetY
-})
-canvas.addEventListener('mouseup', (e) => {
-    if (!body) return
-    body.velocity.x = (body.x - e.offsetX) * 0.05
-    body.velocity.y = (body.y - e.offsetY) * 0.05
-    simulation.addBody(e.offsetX, e.offsetY, { x: body.velocity.x, y: body.velocity.y }, body.mass, body.frozen, body.color)
-})
-
 
 let fps = 60;
 let frameRate = 1000 / fps;
@@ -124,7 +174,7 @@ let timeSinceLastFrame = 0;
 function animate() {
     timeSinceLastFrame = Date.now() - last;
     if (timeSinceLastFrame > frameRate) {
-        simulation.update(0.5);
+        simulation.update(frameRate);
         simulation.draw();
 
         last = Date.now();
